b4f33f8e77c81b1c7b2e79a4ecb0a80641ac447e
Nebot774 <adriclemenbot@gmail.com>
2023-10-31 17:34:35 +0100

    fragments casi echo

---
+++ app/src/main/java/com/example/fragments/ui/theme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fragments.ui.theme;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fragments.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
